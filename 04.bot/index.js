e4bbbc1ca4ab3f4fa302cb7057a3a3098e6b7b51
Jesus Seijas <jesus.seijas@axa.com>
2020-11-09 16:01:56 +0100

    feat: bot

--- 04.bot/index.js
+++ 04.bot/index.js
@@ -1,1 +1,12 @@
-require('@nlpjs/basic').dockStart();
+const { dockStart } = require('@nlpjs/basic');
+
+(async () => {
+  const dock = await dockStart();
+  const bot = dock.get('bot');
+  bot.registerAction('uppers', (session, context, params) => {
+    const variableName = params && params[0] ? params[0] : undefined;
+    if (variableName) {
+      context[variableName] = (context[variableName] || '').toUpperCase();
+    }
+  });
+})();
